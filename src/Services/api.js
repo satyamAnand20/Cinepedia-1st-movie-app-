677532ffffc88909b377d5541d0cb661b59cc6a0
Satyam Anand <satyamanand2012@gmail.com>
2025-07-26 12:27:30 +0530

    UI updates

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
 const BASE_URL = "https://www.omdbapi.com/";
 
-const keywords = ["fight", "adventure", "saga", "kung-fu", "fast"];
+const keywords = ["fight", "adventure", "saga", "kung-fu"];
 
 export const getPopularMovies = async () => {
   let allMovies = [];
